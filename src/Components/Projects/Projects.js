62459f8b2a77514f7113f72c9f4344633052363f
krishna <krishna.kondoju007@gmail.com>
2018-02-10 22:35:35 -0500

    files added

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Project extends Component {
+    render() {
+        return (
+            <div>
+             this is projects    
+            </div>
+        )
+    }
+}
+
+export default Project
