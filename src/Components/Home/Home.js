70c94ebbe94f30f8b61ccafba316be139f19fceb
krishna <krishna.kondoju007@gmail.com>
2018-02-11 02:11:15 -0500

    objects added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 
 class Home extends Component {
     render() {
+        //if statements
+        //ternery opertators
     var user = {
         Firstname: "krishna",
+        first: "kanth",
+        Lastname: "konodju",
+        age: "23"
+    }
+    function getLastname(Lastname){
+        if (Lastname) {
+            return Lastname;
+        }else{
+            return 'unKnown';
+        }
     }
         return (
            
             <div>
-                this is{user.Firstname}
+                My firstname is {user.Firstname ? user.Firstname : user.first} <br/>
+                My lastname is {getLastname(user.Lastname)}
+  {(user.age && user.age <= 18) && user.age}
             </div>
         )
     }
